3c4477d0f99bbbb59edc40b6c7d49a97f8cc9f0a
Zeke <Ipinoluwaheze@gmail.com>
2023-12-29 20:59:23 -0500

    added the reverse.js file

---
+++ reverse.js
@@ -0,0 +1,12 @@
+//This is a function that takes string as a parameter, then returns a reversed version of the string.
+
+function reverse(str){
+	return str.split('').reverse().join('')
+}
+
+
+/*This code uses three methods : .split() , .reverse(), .join()
+ * the .split() method converts the string to an array
+ * the .reverse() method reverses the array
+ * the .join() method changes the array back to a string
+ */
